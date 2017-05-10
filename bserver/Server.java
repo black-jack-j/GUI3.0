5f20a468380e3baf53ee0603259c5e6fbc41c7b2
Yunir Salimzyanov <yunir.vw@gmail.com>
2017-05-11 00:50:09 +0300

    tryToConnect to DB with auth method in Server.java

--- bserver/Server.java
+++ bserver/Server.java
@@ -3,6 +3,10 @@ package bserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -61,5 +65,21 @@ public class Server {
 	public static void main(String[] args){
 		Server s = new Server(1488);
 		s.start();
+	}
+
+	public static int tryToConnect(Connection db, String name, String password){
+		int result = -1;
+		Statement st;
+		try {
+			st = db.createStatement();
+			ResultSet rs = st.executeQuery("SELECT id, isLoged(login, password) FROM users;");
+			while(rs.next()){
+				System.out.println(rs.getString(1) + " " + rs.getString(2));
+				if (rs.getString(2).equals("t")) result = Integer.parseInt(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
